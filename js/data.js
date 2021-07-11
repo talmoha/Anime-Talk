1bce10d6a5f27ae26f449bd64447766597c5c0a6
talmoha <71097072+talmoha@users.noreply.github.com>
2021-07-11 01:05:28 -0400

    forum

---
+++ js/data.js
@@ -0,0 +1,47 @@
+var threads = [
+    {
+        id: 1,
+        title: "1. Who's your favorite anime character?",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "levi"
+            },
+            {
+                author: "Amy",
+                date: Date.now(),
+                content: "pikachu"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "2. Let's discuss the AOT ending",
+        author: "Aaron",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "yes let's"
+            },
+            {
+                author: "Amy",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Jade",
+                date: Date.now(),
+                content: "yup"
+            }
+        ]
+    }
+]
+
+
